ab38b0daccc20befb6bee3e8de3043652472e7ef
CannibalVox <sdbaynham@gmail.com>
2015-03-16 20:35:21 -0500

    Add water walking to tribal boots

--- src/main/java/sct/hexxitgear/item/ItemTribalArmor.java
+++ src/main/java/sct/hexxitgear/item/ItemTribalArmor.java
@@ -20,13 +20,18 @@ package sct.hexxitgear.item;
 
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
 import net.minecraft.client.model.ModelBiped;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.StatCollector;
+import net.minecraft.world.World;
 import sct.hexxitgear.HexxitGear;
+import sct.hexxitgear.core.ArmorSet;
 import sct.hexxitgear.model.ModelSkullHelmet;
 import sct.hexxitgear.util.FormatCodes;
 
@@ -60,5 +65,42 @@ public class ItemTribalArmor extends ItemHexxitArmor {
     @Override
     public void addInformation(ItemStack par1ItemStack, EntityPlayer par2EntityPlayer, List infoList, boolean par4) {
         infoList.add(FormatCodes.Indigo.format + StatCollector.translateToLocal("gui.hexxitgear.set.tribal"));
+    }
+
+    @Override
+    public void onArmorTick(World world, EntityPlayer player, ItemStack itemStack)
+    {
+        super.onArmorTick(world, player, itemStack);
+
+        if (itemStack.getItem() == HexxitGear.tribalShoes) {
+            shoesWaterWalkCheck(world, player);
+        }
+    }
+
+    private void shoesWaterWalkCheck(World world, EntityPlayer player) {
+        int x = (int) Math.floor(player.posX);
+        int y = (int) (player.posY - player.getYOffset());
+        int z = (int) Math.floor(player.posZ);
+
+        Block floor = world.getBlock(x, y - 1, z);
+        Block playerLegs = world.getBlock(x, y, z);
+
+        if (!playerLegs.isAir(world, x, y, z)) {
+            return;
+        }
+
+        boolean hasFullSet = (ArmorSet.getPlayerArmorSet(player.getDisplayName()) != null);
+        boolean floorIsGood = false;
+
+        if (floor.getMaterial() == Material.water)
+            floorIsGood = true;
+        else if (hasFullSet && floor.getMaterial() == Material.lava)
+            floorIsGood = true;
+
+        if (floorIsGood && !player.isSneaking() && (hasFullSet || player.isSprinting())) {
+            player.motionY = 0.0d;
+            player.fallDistance = 0.0f;
+            player.onGround = true;
+        }
     }
 }
